37391bfdc4358619ab0bf90c1560da3d0f33318b
abulyousuf <abulfmyousuf@gmail.com>
2022-09-18 18:12:22 +0100

    add an error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -39,7 +40,11 @@ class Details extends Component {
 const WrappedDetails = () => {
   const params = useParams();
 
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />;
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
